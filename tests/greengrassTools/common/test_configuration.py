995ddaa60237b65bf23badc3870e969e75777022
Pallavi Bharane <bharanep@amazon.com>
2021-10-20 18:45:07 -0700

    test: added missing test for configuration not found
    cr: https://code.amazon.com/reviews/CR-59126393

--- tests/greengrassTools/common/test_configuration.py
+++ tests/greengrassTools/common/test_configuration.py
@@ -9,6 +9,9 @@ def test_get_configuration_valid_component_config_found():
     expected_config = json.loads(component_config)
     assert config.get_configuration(Path(".").joinpath('tests/greengrassTools/static').joinpath('config.json').resolve(), "1") == expected_config
 
+def test_get_configuration_valid_component_config_no_found():
+    assert config.get_configuration(Path(".").joinpath('tests/greengrassTools/static').joinpath('config.json').resolve(), "2") is None
+
 @pytest.mark.parametrize("file_name", ["invalid_config.json", "invalid_case_config.json", "invalid_version_config.json"])
 def test_get_configuration_invalid_config_file(file_name):
     with pytest.raises(jsonschema.exceptions.ValidationError) as err:
