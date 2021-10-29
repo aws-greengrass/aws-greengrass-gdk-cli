1178fb46fbeda81f9cc65792fb31ffd172770a07
saranya <nukai@amazon.com>
2021-10-28 19:06:41 -0700

    refactor: validate no of components and build command in config
    cr: https://code.amazon.com/reviews/CR-59607275

--- tests/greengrassTools/common/test_configuration.py
+++ tests/greengrassTools/common/test_configuration.py
@@ -7,14 +7,14 @@ import greengrassTools.common.utils as utils
 import greengrassTools.common.consts as consts
 
 def test_get_configuration_valid_component_config_found(mocker):
-    expected_config = {"component" :{"1": {"author": "abc","version": "1.0.0","build": {"command" : "default"},"publish": {"bucket_name": "default"}}},"tools-version": "1.0.0"}
+    expected_config = {"component" :{"1": {"author": "abc","version": "1.0.0","build": {"command" : ["default"]},"publish": {"bucket_name": "default"}}},"tools_version": "1.0.0"}
 
     mock__get_project_config_file=mocker.patch("greengrassTools.common.configuration._get_project_config_file", 
     return_value=Path(".").joinpath('tests/greengrassTools/static').joinpath('config.json'))
     assert config.get_configuration() == expected_config
     assert mock__get_project_config_file.called
 
-@pytest.mark.parametrize("file_name", ["invalid_config.json", "invalid_case_config.json", "invalid_version_config.json"])
+@pytest.mark.parametrize("file_name", ["invalid_config.json", "invalid_case_config.json", "invalid_version_config.json", "invalid_multiple_components.json","invalid_build_command.json"])
 def test_get_configuration_invalid_config_file(mocker, file_name):
     mock__get_project_config_file=mocker.patch("greengrassTools.common.configuration._get_project_config_file", 
     return_value=Path(".").joinpath('tests/greengrassTools/static').joinpath(file_name).resolve())
