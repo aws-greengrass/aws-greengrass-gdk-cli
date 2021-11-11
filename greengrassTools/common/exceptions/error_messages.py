3604fe57e6ad81d9afc0dab0fa24a8fb04ebfd3f
saranya <nukai@amazon.com>
2021-11-11 13:43:23 -0800

    feat(init): initializes project based on args

--- greengrassTools/common/exceptions/error_messages.py
+++ greengrassTools/common/exceptions/error_messages.py
@@ -10,3 +10,16 @@ PROJECT_CONFIG_FILE_INVALID = "Project configuration file '{}' is invalid. Pleas
 CLI_MODEL_FILE_NOT_EXISTS = "Model validation failed. CLI model file doesn't exist."
 INVALID_CLI_MODEL = "CLI model is invalid. Please provide a valid model to create the CLI parser."
 LISTING_COMPONENTS_FAILED = "Failed to list the available components from GitHub. Please try again after sometime."
+INIT_FAILS_DURING_COMPONENT_DOWNLOAD = "Failed to download the selected component {{}}. Please try again after sometime."
+INIT_WITH_INVALID_ARGS = (
+    "Could not initialize the project as the arguments passed are invalid. Please initialize the project with correct"
+    " args.\nTry `greengrass-tools component init --help`"
+)
+INIT_WITH_CONFLICTING_ARGS = (
+    "Could not initialize the project as the command args are conflicting. Please initialize the project with correct"
+    " args.\nTry `greengrass-tools component init --help` "
+)
+INIT_NON_EMPTY_DIR_ERROR = (
+    "Could not initialize the project as the directory is not empty. Please initialize the project in an empty directory.\nTry"
+    " `greengrass-tools component init --help`"
+)
