caf4689c2489b228c8977209743f917762ce4539
Saranya <nukai@amazon.com>
2022-02-02 17:18:21 -0800

    test(uat): build with no artifacts + use fixture (#59)

--- uat/test_uat_CLIParser.py
+++ uat/test_uat_CLIParser.py
@@ -1,8 +1,15 @@
 import json
 import os
 import subprocess as sp
-import tempfile
 from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def change_test_dir(tmpdir, monkeypatch):
+    monkeypatch.chdir(tmpdir)
+    return tmpdir
 
 
 def test_list_template():
@@ -22,36 +29,54 @@ def test_init_template_non_empty_dir():
     assert "Try `gdk component init --help`" in check_init_template.stdout.decode()
 
 
-def test_init_template():
-    dirpath = tempfile.mkdtemp()
-    os.chdir(dirpath)
+def test_init_template(change_test_dir):
+    dirpath = Path(change_test_dir)
     check_init_template = sp.run(["gdk", "component", "init", "-t", "HelloWorld", "-l", "python"], check=True, stdout=sp.PIPE)
     assert check_init_template.returncode == 0
     assert Path(dirpath).joinpath("recipe.yaml").resolve().exists()
     assert Path(dirpath).joinpath("gdk-config.json").resolve().exists()
 
 
-def test_init_repository():
-    dirpath = tempfile.mkdtemp()
-    os.chdir(dirpath)
+def test_init_template_with_new_directory(change_test_dir):
+    dir = "test-dir"
+    dirpath = Path(change_test_dir).joinpath(dir)
+    check_init_template = sp.run(
+        ["gdk", "component", "init", "-t", "HelloWorld", "-l", "python", "-n", dir], check=True, stdout=sp.PIPE
+    )
+    assert check_init_template.returncode == 0
+    assert Path(dirpath).joinpath("recipe.yaml").resolve().exists()
+    assert Path(dirpath).joinpath("gdk-config.json").resolve().exists()
+
+
+def test_init_repository(change_test_dir):
+    dirpath = Path(change_test_dir)
     check_init_repo = sp.run(
         ["gdk", "component", "init", "-r", "aws-greengrass-labs-database-influxdb"], check=True, stdout=sp.PIPE
     )
-
     assert check_init_repo.returncode == 0
     assert Path(dirpath).joinpath("recipe.yaml").exists()
     assert Path(dirpath).joinpath("gdk-config.json").exists()
 
 
-def test_build_template_zip():
-    dirpath = tempfile.mkdtemp()
+def test_init_repository_with_new_dir(change_test_dir):
+    dir = "test-dir"
+    dirpath = Path(change_test_dir).joinpath(dir)
+    check_init_repo = sp.run(
+        ["gdk", "component", "init", "-r", "aws-greengrass-labs-database-influxdb", "-n", dir], check=True, stdout=sp.PIPE
+    )
+    assert check_init_repo.returncode == 0
+    assert Path(dirpath).joinpath("recipe.yaml").exists()
+    assert Path(dirpath).joinpath("gdk-config.json").exists()
+
+
+def test_build_template_zip(change_test_dir):
     # Recipe contains HelloWorld.zip artifact. So, create HelloWorld directory inside temporary directory.
-    path_HelloWorld = Path(dirpath).joinpath("HelloWorld")
-    os.mkdir(path_HelloWorld)
-    os.chdir(path_HelloWorld)
+    path_HelloWorld = Path(change_test_dir).joinpath("HelloWorld")
 
     # Check if init downloads templates with necessary files.
-    check_init_template = sp.run(["gdk", "component", "init", "-t", "HelloWorld", "-l", "python"], check=True, stdout=sp.PIPE)
+    check_init_template = sp.run(
+        ["gdk", "component", "init", "-t", "HelloWorld", "-l", "python", "-n", "HelloWorld"], check=True, stdout=sp.PIPE
+    )
     assert check_init_template.returncode == 0
     assert Path(path_HelloWorld).joinpath("recipe.yaml").resolve().exists()
     config_file = Path(path_HelloWorld).joinpath("gdk-config.json").resolve()
@@ -64,6 +89,7 @@ def test_build_template_zip():
     with open(str(config_file), "w") as f:
         f.write(json.dumps(config))
 
+    os.chdir(path_HelloWorld)
     # Check if build works as expected.
     check_build_template = sp.run(["gdk", "component", "build"], check=True, stdout=sp.PIPE)
     assert check_build_template.returncode == 0
@@ -80,3 +106,39 @@ def test_build_template_zip():
     )
 
     assert artifact_path.exists()
+
+
+def test_build_template_zip_fail_with_no_artifact(change_test_dir):
+    # Recipe contains HelloWorld.zip artifact. So, create HelloWorld directory inside temporary directory.
+    dir_path = Path(change_test_dir).joinpath("artifact-not-exists")
+
+    # Check if init downloads templates with necessary files.
+    check_init_template = sp.run(
+        ["gdk", "component", "init", "-t", "HelloWorld", "-l", "python", "-n", "artifact-not-exists"],
+        check=True,
+        stdout=sp.PIPE,
+    )
+    assert check_init_template.returncode == 0
+    assert Path(dir_path).joinpath("recipe.yaml").resolve().exists()
+    config_file = Path(dir_path).joinpath("gdk-config.json").resolve()
+    assert config_file.exists()
+
+    # Update gdk-config file mandatory field like region.
+    with open(str(config_file), "r") as f:
+        config = json.loads(f.read())
+        config["component"]["com.example.PythonHelloWorld"]["publish"]["region"] = "us-east-1"
+    with open(str(config_file), "w") as f:
+        f.write(json.dumps(config))
+
+    os.chdir(dir_path)
+    # Check if build works as expected.
+    check_build_template = sp.run(["gdk", "component", "build"], stdout=sp.PIPE)
+    output = check_build_template.stdout.decode()
+    assert check_build_template.returncode == 1
+    assert Path(dir_path).joinpath("zip-build").resolve().exists()
+    assert (
+        "Could not find artifact with URI 's3://BUCKET_NAME/COMPONENT_NAME/COMPONENT_VERSION/HelloWorld.zip' on s3 or inside"
+        " the build folders."
+        in output
+    )
+    assert "Failed to build the component with the given project configuration." in output
