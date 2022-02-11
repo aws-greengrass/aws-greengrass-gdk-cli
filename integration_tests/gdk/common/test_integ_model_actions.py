a07d5662f40a2c2ca91105e4becf47193892becb
saranya <nukai@amazon.com>
2022-02-11 00:41:25 -0800

    test(model_actions): add unit & integration tests

--- integration_tests/gdk/common/test_integ_model_actions.py
+++ integration_tests/gdk/common/test_integ_model_actions.py
@@ -40,3 +40,98 @@ def test_cli_model(mocker):
     with open(utils.get_static_file_path(cli_model_schema), "r") as schemaFile:
         schema = json.loads(schemaFile.read())
     jsonschema.validate(cli_data, schema)
+
+
+def test_is_valid_model_without_help_in_command():
+    # Invalid model without help for commands
+    valid_model = {
+        "gdk": {"sub-commands": ["component"]},
+        "component": {"sub-commands": ["init", "build"]},
+        "init": {"arguments": {"lang": {"name": ["-l", "--lang"], "help": "help"}}},
+        "build": {},
+    }
+    assert not model_actions.is_valid_model(valid_model, consts.cli_tool_name)
+
+
+def test_is_valid_model_with_invalid_argument():
+    # Invalid model with incorrect sub-commands. Subcommand with no key in the cli model.
+    invalid_model_without_name_in_args = {
+        "gdk": {
+            "sub-commands": ["component"],
+            "arguments": {"lang": {"names": ["-l", "--lang"], "help": "help"}},
+            "help": "help",
+        },
+        "component": {"help": "help"},
+    }
+    assert not model_actions.is_valid_model(invalid_model_without_name_in_args, consts.cli_tool_name)
+
+
+def test_is_valid_model_with_invalid_sub_command():
+    # Invalid model with incorrect sub-commands. Subcommand with no key in the cli model.
+    invalid_model_subcommands = {
+        "gdk": {"sub-commands": ["component", "invalid-sub-command"]},
+        "component": {},
+    }
+    assert not model_actions.is_valid_model(invalid_model_subcommands, consts.cli_tool_name)
+
+
+def test_is_valid_model_with_invalid_arg_group_missing_title():
+    # Valid model with correct args ang sub-commands.
+    invalid_model = {
+        "gdk": {"sub-commands": ["component"], "help": "help"},
+        "component": {"sub-commands": ["init", "build"], "help": "help"},
+        "init": {
+            "arguments": {"lang": {"name": ["-l", "--lang"], "help": "help"}},
+            "arg_groups": [
+                {
+                    "args": ["lang"],
+                    "description": "description",
+                }
+            ],
+            "help": "help",
+        },
+        "build": {"help": "help"},
+    }
+    assert not model_actions.is_valid_model(invalid_model, consts.cli_tool_name)
+
+
+def test_is_valid_model_with_invalid_arg_group_missing_arg():
+    # Valid model with correct args ang sub-commands.
+    invalid_model = {
+        "gdk": {"sub-commands": ["component"], "help": "help"},
+        "component": {"sub-commands": ["init", "build"], "help": "help"},
+        "init": {
+            "arguments": {"lang": {"name": ["-l", "--lang"], "help": "help"}},
+            "arg_groups": [
+                {
+                    "title": "title",
+                    "args": ["lang", "template"],
+                    "description": "description",
+                }
+            ],
+            "help": "help",
+        },
+        "build": {"help": "help"},
+    }
+    assert not model_actions.is_valid_model(invalid_model, consts.cli_tool_name)
+
+
+def test_is_valid_model_with_valid_cli_model():
+    # Valid model with correct args ang sub-commands.
+    valid_model = {
+        "gdk": {"sub-commands": ["component"], "help": "help"},
+        "component": {"sub-commands": ["init", "build"], "help": "help"},
+        "init": {
+            "help": "help",
+            "arguments": {"lang": {"name": ["-l", "--lang"], "help": "help"}},
+            "arg_groups": [
+                {
+                    "title": "Greengrass component templates.",
+                    "args": ["lang"],
+                    "description": "description",
+                }
+            ],
+        },
+        "build": {"help": "build"},
+    }
+    assert model_actions.is_valid_model(valid_model, consts.cli_tool_name)
