9698b175ef8d143b940f1fffd36c1c772cd108f8
Nelson Ochoa <ochoanel@amazon.com>
2023-02-14 08:06:36 -0700

    test: for coverage

--- tests/gdk/build_system/test_zip_build_configuraiton.py
+++ tests/gdk/build_system/test_zip_build_configuraiton.py
@@ -1,8 +1,10 @@
+from pathlib import Path
 from faker import Faker
 from faker.providers import color
 import pytest
 from unittest import TestCase
 
+from gdk.build_system.Zip import Zip
 from gdk.common.configuration import validate_configuration
 
 
@@ -54,3 +56,12 @@ class ZipConfigurationTests(TestCase):
             options = dict()
             options[fake.color_name] = fake.color_name
             validate_configuration(self.configuration_base(options))
+
+    def test_zip_ignore_list_without_exclude_option(self):
+        # When
+        recipe_path = Path("/src/GDK-CLI-Internal/tests/gdk/static/build_command/recipe.json")
+        zip = Zip({"component_recipe_file": recipe_path}, {Path(".").resolve()})
+
+        # Then
+        assert ["gdk-config.json", "greengrass-build", "recipe.json",
+                "test*", ".*", "node_modules"] == zip._ignore_files_during_zip(None, None)
