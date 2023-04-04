cedd6d06d6d02ec01f0c8a069c28ae84b35dfed2
Saranya <nukai@amazon.com>
2023-04-04 10:26:28 -0700

    feat: add case insensitive utils to the recipe files (#141)

---
+++ gdk/common/CaseInsensitive.py
@@ -0,0 +1,98 @@
+import json
+import yaml
+from pathlib import Path
+
+from requests.structures import CaseInsensitiveDict as _CaseInsensitiveDict
+
+
+class CaseInsensitiveDict(_CaseInsensitiveDict):
+    def __init__(self, data=None, **kwargs):
+        super().__init__(data, **kwargs)
+        _dict = _CaseInsensitiveDict(data)
+        self._convert_nested_dict(_dict)
+        self.update(_dict)
+
+    def to_dict(self):
+        _dict = dict(self)
+        self._convert_nested_case_insensitive_dict(_dict)
+        return _dict
+
+    def update_value(self, key, value):
+        if key.lower() in self._store:
+            key = self._store[key.lower()][0]
+        self._store[key.lower()] = (key, value)
+
+    def _convert_nested_dict(self, case_insensitive_dict: _CaseInsensitiveDict) -> None:
+        for key, value in case_insensitive_dict.items():
+            if isinstance(value, dict):
+                case_insensitive_dict.update({key: CaseInsensitiveDict(value)})
+            elif isinstance(value, list):
+                case_insensitive_dict.update({key: [CaseInsensitiveDict(val) for val in value if isinstance(val, dict)]})
+
+    def _convert_nested_case_insensitive_dict(self, dictObj: dict) -> dict:
+        for key, value in dictObj.items():
+            if isinstance(value, CaseInsensitiveDict):
+                dictObj.update({key: self._convert_nested_case_insensitive_dict(dict(value))})
+            elif isinstance(value, list):
+                dictObj.update(
+                    {
+                        key: [
+                            self._convert_nested_case_insensitive_dict(dict(val))
+                            for val in value
+                            if isinstance(val, CaseInsensitiveDict)
+                        ]
+                    }
+                )
+        return dictObj
+
+
+class CaseInsensitiveRecipeFile:
+    def write(self, file_path: Path, content: CaseInsensitiveDict) -> None:
+        """
+        Writes CaseInsensitiveDict contents to a JSON or a YAML file based on the file path.
+        """
+        if not self._is_json(file_path) and not self._is_yaml(file_path):
+            raise Exception(f"Invalid recipe file : {file_path}. Recipe file must be in json or yaml format.")
+        self._write(file_path, content.to_dict())
+
+    def read(self, file_path: Path) -> CaseInsensitiveDict:
+        """
+        Reads a JSON or a YAMl file contents as a CaseInsensitiveDict and returns it.
+        """
+        if not self._is_json(file_path) and not self._is_yaml(file_path):
+            raise Exception(f"Invalid recipe file : {file_path}. Recipe file must be in json or yaml format.")
+        return CaseInsensitiveDict(self._read(file_path))
+
+    def _write(self, file_path, content):
+        if self._is_json(file_path):
+            self._write_to_json(file_path, content)
+        else:
+            self._write_to_yaml(file_path, content)
+
+    def _read(self, file_path):
+        if self._is_json(file_path):
+            return self._read_from_json(file_path)
+        else:
+            return self._read_from_yaml(file_path)
+
+    def _read_from_yaml(self, file_path: Path) -> dict:
+        with open(file_path, "r", encoding="utf-8") as f:
+            return yaml.safe_load(f.read())
+
+    def _read_from_json(self, file_path: Path) -> dict:
+        with open(file_path, "r", encoding="utf-8") as f:
+            return json.loads(f.read())
+
+    def _write_to_json(self, file_path: Path, content: dict) -> None:
+        with open(file_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(content, indent=4))
+
+    def _write_to_yaml(self, file_path: Path, content: dict) -> None:
+        with open(file_path, "w", encoding="utf-8") as f:
+            yaml.safe_dump(content, f, sort_keys=False)
+
+    def _is_json(self, file_path: Path) -> bool:
+        return file_path.name.endswith(".json")
+
+    def _is_yaml(self, file_path: Path) -> bool:
+        return file_path.name.endswith(".yaml") or file_path.name.endswith(".yml")
---
+++ integration_tests/gdk/common/test_CaseInsensitive.py
@@ -0,0 +1,104 @@
+from pathlib import Path
+from unittest import TestCase
+import tempfile
+import pytest
+import yaml
+from gdk.common.CaseInsensitive import CaseInsensitiveRecipeFile
+import json
+from gdk.common.CaseInsensitive import CaseInsensitiveDict
+
+
+class CaseInsensitiveRecipeFileTest(TestCase):
+    @pytest.fixture(autouse=True)
+    def __inject_fixtures(self, mocker):
+        self.mocker = mocker
+
+    def test_read_json(self):
+        json_file = Path(".").joinpath("tests/gdk/static/project_utils").joinpath("valid_component_recipe.json").resolve()
+        case_insensitive_recipe = CaseInsensitiveRecipeFile().read(json_file)
+        assert isinstance(CaseInsensitiveRecipeFile().read(json_file), CaseInsensitiveDict)
+        assert "manifests" in case_insensitive_recipe
+        assert "MANIFESTS" in case_insensitive_recipe
+        assert "artifacts" in case_insensitive_recipe["manifests"][0]
+        assert "ARTIFACTS" in case_insensitive_recipe["manifests"][0]
+        assert "uri" in case_insensitive_recipe["Manifests"][0]["Artifacts"][0]
+        assert "URI" in case_insensitive_recipe["Manifests"][0]["Artifacts"][0]
+
+    def test_read_yaml(self):
+        yaml_file = Path(".").joinpath("tests/gdk/static/project_utils").joinpath("valid_component_recipe.yaml").resolve()
+        case_insensitive_recipe = CaseInsensitiveRecipeFile().read(yaml_file)
+        assert isinstance(case_insensitive_recipe, CaseInsensitiveDict)
+        assert "manifests" in case_insensitive_recipe
+        assert "MANIFESTS" in case_insensitive_recipe
+        assert "artifacts" in case_insensitive_recipe["manifests"][0]
+        assert "ARTIFACTS" in case_insensitive_recipe["manifests"][0]
+        assert "uri" in case_insensitive_recipe["Manifests"][0]["Artifacts"][0]
+        assert "URI" in case_insensitive_recipe["Manifests"][0]["Artifacts"][0]
+
+    def test_read_invalid_format(self):
+        invalid_file = Path(".").joinpath("tests/gdk/static/project_utils").joinpath("not_exists.txt").resolve()
+        with pytest.raises(Exception) as e:
+            CaseInsensitiveRecipeFile().read(invalid_file)
+        assert "Recipe file must be in json or yaml format" in e.value.args[0]
+
+    def test_write_json(self):
+        json_file = Path(".").joinpath("tests/gdk/static/project_utils").joinpath("valid_component_recipe.json").resolve()
+        with tempfile.TemporaryDirectory() as newDir:
+            tmp_path = Path(newDir).joinpath("valid.json").resolve()
+            CaseInsensitiveRecipeFile().write(tmp_path, CaseInsensitiveRecipeFile().read(json_file))
+            with open(json_file, "r") as original_yaml:
+                with open(tmp_path, "r") as updated_yaml:
+                    assert json.loads(original_yaml.read()) == json.loads(updated_yaml.read())
+
+    def test_write_yaml(self):
+        yaml_file = Path(".").joinpath("tests/gdk/static/project_utils").joinpath("valid_component_recipe.yaml").resolve()
+        with tempfile.TemporaryDirectory() as newDir:
+            tmp_path = Path(newDir).joinpath("valid.yaml").resolve()
+            CaseInsensitiveRecipeFile().write(tmp_path, CaseInsensitiveRecipeFile().read(yaml_file))
+            with open(yaml_file, "r") as original_yaml:
+                with open(tmp_path, "r") as updated_yaml:
+                    assert yaml.safe_load(original_yaml.read()) == yaml.safe_load(updated_yaml.read())
+
+    def test_write_invalid_format(self):
+        with tempfile.TemporaryDirectory() as newDir:
+            yaml_file = Path(".").joinpath("tests/gdk/static/project_utils").joinpath("valid_component_recipe.yaml").resolve()
+            contents = CaseInsensitiveRecipeFile().read(yaml_file)
+            tmp_path = Path(newDir).joinpath("invalid.txt").resolve()
+            with pytest.raises(Exception) as e:
+                CaseInsensitiveRecipeFile().write(tmp_path, contents)
+            assert "Recipe file must be in json or yaml format" in e.value.args[0]
+
+
+class CaseInsensitiveDictTest(TestCase):
+    @pytest.fixture(autouse=True)
+    def __inject_fixtures(self, mocker):
+        self.mocker = mocker
+
+    def test_convert_dict_to_CaseInsensitiveDict(self):
+        dictionary = {
+            "key1": "value1",
+            "key2": [{"key21": "value21"}, {"key22": "value22"}],
+            "key3": {"key31": {"key311": "key312"}},
+        }
+        cis = CaseInsensitiveDict(dictionary)
+        assert "KEY1" in cis
+        assert cis["KEY2"][0]["KEy21"] == "value21"
+        assert cis["KEy3"]["key31"]["KeY311"] == "key312"
+        assert cis.to_dict() == dictionary
+
+    def test_when_update_value_then_key_not_changed(self):
+        dictionary = {
+            "key1": "value1",
+            "key2": [{"key21": "value21"}, {"key22": "value22"}],
+            "key3": {"key31": {"key311": "key312"}},
+        }
+        cis = CaseInsensitiveDict(dictionary)
+        cis.update_value("KEY1", "updated-value")
+        cis["keY2"][0].update_value("KEY21", "updated-value21")
+
+        assert cis["KEY1"] == "updated-value"
+        assert cis.to_dict() == {
+            "key1": "updated-value",
+            "key2": [{"key21": "updated-value21"}, {"key22": "value22"}],
+            "key3": {"key31": {"key311": "key312"}},
+        }
