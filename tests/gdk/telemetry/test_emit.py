d5368d84161409f2954c343b2af6afe48a495208
Nelson Ochoa <ochoanel@amazon.com>
2023-04-25 08:07:00 -0600

    feat: telemetry prompt (#166)
    display telemetry prompt to the user before sending any telemetry information

--- tests/gdk/telemetry/test_emit.py
+++ tests/gdk/telemetry/test_emit.py
@@ -1,17 +1,40 @@
+import contextlib
+import io
 
-from unittest import TestCase
-
-from gdk.telemetry.emit import Emit
+from gdk.telemetry.emit import TELEMETRY_PROMPT, Emit
 from gdk.telemetry.metric import MetricType
+from integration_tests.gdk.telemetry.telemetry_base import TelemetryTestCase
 from tests.helpers.telemetry_fake import TelemetryFake
 
 
-class TestEmit(TestCase):
+class TestEmit(TelemetryTestCase):
 
     def setUp(self) -> None:
+        super().setUp()
         self.telemetry = TelemetryFake()
 
+    def test_display_telemetry_prompt_only_once_telemetry_enabled(self):
+        self.enable_telemetry()
+        stdout = io.StringIO()
+
+        with contextlib.redirect_stderr(stdout):
+            Emit(self.telemetry).installed_metric()
+
+        captured_out = stdout.getvalue()
+        self.assertEqual(TELEMETRY_PROMPT.strip(), captured_out.strip())
+
+    def test_display_telemetry_prompt_telemetry_disabled(self):
+        self.disable_telemetry()
+        stdout = io.StringIO()
+
+        with contextlib.redirect_stderr(stdout):
+            Emit(self.telemetry).installed_metric()
+
+        captured_out = stdout.getvalue()
+        self.assertEqual("", captured_out.strip())
+
     def test_emitting_installed_metric(self):
+        self.enable_telemetry()
         Emit(self.telemetry).installed_metric()
 
         emitted = self.telemetry.get_emitted_metrics()
