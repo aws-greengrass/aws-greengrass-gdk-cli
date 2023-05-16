8b121c8cb9706f1b2d433ca298e2150965c72f61
Saranya <nukai@amazon.com>
2023-05-16 10:51:31 -0700

    feat: implement gdk build command (#179)

---
+++ gdk/commands/test/BuildCommand.py
@@ -0,0 +1,106 @@
+from gdk.commands.Command import Command
+from gdk.common.config.GDKProject import GDKProject
+from gdk.build_system.UATBuildSystem import UATBuildSystem
+from pathlib import Path
+import gdk.common.utils as utils
+import shutil
+import logging
+from gdk.common.CaseInsensitive import CaseInsensitiveRecipeFile
+
+
+class BuildCommand(Command):
+    def __init__(self, command_args) -> None:
+        super().__init__(command_args, "build")
+        self._gdk_project = GDKProject()
+        self._test_config = self._gdk_project.test_config
+        self.test_directory = utils.get_current_directory().joinpath("uat-features")
+        self.recipe_file_name = self._gdk_project.recipe_file.name
+        self._gg_build_uat_dir = self._gdk_project.gg_build_dir.joinpath("uat-features")
+        self.should_create_uat_recipe = False
+
+    def run(self):
+        """
+        This method is called when customer runs the `gdk test build` command.
+
+        1. Remove the UAT module from greengrass-build if it exists.
+        2. Copy the UAT folder to the greengrass-build directory and perform the following steps:
+        3. Update the feature files with component name and UAT recipe file path.
+        4. Create the UAT recipe file in the greengrass-build folder.
+        5. Build the UAT module.
+        """
+        build_recipe_file = self._gdk_project.gg_build_recipes_dir.joinpath(self.recipe_file_name)
+        uat_recipe_file = self._gdk_project.gg_build_recipes_dir.joinpath("uat_" + self.recipe_file_name)
+        self._clean_uat_build_dir()
+        self._copy_uat_dir_to_build()
+        self.update_feature_files(build_recipe_file, uat_recipe_file)
+        self.create_uat_recipe_file(build_recipe_file, uat_recipe_file)
+        self.build_uat_module()
+
+    def _clean_uat_build_dir(self):
+        if self._gg_build_uat_dir.exists():
+            logging.debug("Removing the UAT module from greengrass-build directory")
+            shutil.rmtree(self._gg_build_uat_dir)
+
+    def build_uat_module(self):
+        logging.info("Building the UAT module")
+        build_system = UATBuildSystem.get(self._test_config.test_build_system)
+        build_system.build(self._gg_build_uat_dir)
+
+    def _copy_uat_dir_to_build(self):
+        logging.debug("Copying the UAT module to greengrass-build directory")
+        shutil.copytree(self.test_directory, self._gg_build_uat_dir)
+
+    def update_feature_files(self, build_recipe_file: Path, uat_recipe_file: Path):
+        """
+        Update .feature files that have GDK_COMPONENT_NAME and/or GDK_COMPONENT_RECIPE_FILE variables with the component name
+        and uat_recipe file path respectively.
+
+        If a feature file contains GDK_COMPONENT_RECIPE_FILE variable, the component must be built before building the test
+        module or an exception is thrown.
+        """
+        feature_files = list(self._gg_build_uat_dir.rglob("*.feature"))
+        for feature_file in feature_files:
+            with open(feature_file, "r", encoding="utf-8") as f:
+                feature_file_content = f.read()
+
+            if "GDK_COMPONENT_NAME" not in feature_file_content and "GDK_COMPONENT_RECIPE_FILE" not in feature_file_content:
+                continue
+
+            if "GDK_COMPONENT_NAME" in feature_file_content:
+                feature_file_content = feature_file_content.replace("GDK_COMPONENT_NAME", self._gdk_project.component_name)
+
+            if "GDK_COMPONENT_RECIPE_FILE" in feature_file_content:
+                if not build_recipe_file.exists():
+                    raise Exception(
+                        "Component is not built. Build it with `gdk component build` command before building the testing"
+                        " module."
+                    )
+                else:
+                    self.should_create_uat_recipe = True
+                    feature_file_content = feature_file_content.replace("GDK_COMPONENT_RECIPE_FILE", uat_recipe_file.as_uri())
+            logging.info("Updating feature file: %s", feature_file.as_uri())
+            with open(feature_file, "w", encoding="utf-8") as f:
+                f.write(feature_file_content)
+
+    def create_uat_recipe_file(self, build_recipe_file: Path, uat_recipe_file: Path) -> None:
+        """
+        When the component is built using `gdk component build` command gdk creates a build recipe file. This method uses that
+        build recipe file and creates a uat_recipe file in the greengrass-build/recipes folder by replacing the s3 artifact
+        URIs with their absolute file paths.
+        """
+        if not self.should_create_uat_recipe:
+            return
+
+        _recipe = CaseInsensitiveRecipeFile().read(build_recipe_file)
+        for manifest in _recipe.get("manifests", []):
+            for artifact in manifest.get("artifacts", []):
+                artifact_uri = artifact.get("uri", "")
+                if not artifact_uri.startswith(utils.s3_prefix):
+                    continue
+                artifact_path = self._gdk_project.gg_build_component_artifacts_dir.joinpath(Path(artifact_uri).name).resolve()
+                if not artifact_path.exists():
+                    logging.debug("Artifact %s does not exist in the build directory", artifact_uri)
+                    continue
+                artifact.update_value("Uri", artifact_path.as_uri())
+        logging.info("Creating the UAT recipe file: %s", uat_recipe_file.resolve())
+        CaseInsensitiveRecipeFile().write(uat_recipe_file, _recipe)
---
+++ tests/gdk/commands/test/test_uat_BuildCommand.py
@@ -0,0 +1,200 @@
+import pytest
+from unittest import TestCase
+from unittest.mock import call, ANY
+from gdk.commands.test.BuildCommand import BuildCommand
+from pathlib import Path
+from unittest import mock
+import platform
+from gdk.common.CaseInsensitive import CaseInsensitiveDict, CaseInsensitiveRecipeFile
+import os
+
+
+class BuildCommandUnitTest(TestCase):
+    @pytest.fixture(autouse=True)
+    def __inject_fixtures(self, mocker, tmpdir):
+        self.mocker = mocker
+        self.tmpdir = tmpdir
+        self.c_dir = Path(".").resolve()
+
+        config = {
+            "component": {
+                "abc": {
+                    "author": "abc",
+                    "version": "1.0.0",
+                    "build": {"build_system": "zip"},
+                    "publish": {"bucket": "default", "region": "us-east-1"},
+                }
+            }
+        }
+
+        self.mocker.patch("gdk.common.configuration.get_configuration", return_value=config)
+        self.mocker.patch("gdk.commands.component.project_utils.get_recipe_file", return_value=Path("."))
+        os.chdir(tmpdir)
+        yield
+        os.chdir(self.c_dir)
+
+    def test_update_features_with_no_interpolation(self):
+        build_cmd = BuildCommand({})
+        mock_rglob = self.mocker.patch("pathlib.Path.rglob", return_value=[Path("abc.feature")])
+
+        with mock.patch("builtins.open", mock.mock_open(read_data="nothing_to_update")) as mock_file:
+            build_cmd.update_feature_files("recipe.yaml", "uat_recipe.yaml")
+            assert mock_file.call_args_list == [call(Path(".").joinpath("abc.feature"), "r", encoding="utf-8")]
+
+        assert mock_rglob.call_args_list == [call("*.feature")]
+
+    def test_update_features_with_component_name_interpolation(self):
+        self.mocker.patch("pathlib.Path.as_uri", return_value="file:///abc.feature")
+        mock_rglob = self.mocker.patch("pathlib.Path.rglob", return_value=[Path("abc.feature")])
+        build_cmd = BuildCommand({})
+
+        with mock.patch("builtins.open", mock.mock_open(read_data="This is GDK_COMPONENT_NAME")) as mock_file:
+            build_cmd.update_feature_files("recipe.yaml", "uat_recipe.yaml")
+            assert mock_file.call_args_list == [
+                call(Path(".").joinpath("abc.feature"), "r", encoding="utf-8"),
+                call(Path(".").joinpath("abc.feature"), "w", encoding="utf-8"),
+            ]
+
+            assert mock_file.return_value.write.call_args_list == [call("This is abc")]
+
+        assert mock_rglob.call_args_list == [call("*.feature")]
+
+    def test_update_features_with_component_recipe_file_interpolation(self):
+        mock_rglob = self.mocker.patch("pathlib.Path.rglob", return_value=[Path("abc.feature")])
+        self.mocker.patch("pathlib.Path.exists", return_value=True)
+        self.mocker.patch("pathlib.Path.as_uri", return_value="file:///abc.feature")
+        build_cmd = BuildCommand({})
+
+        with mock.patch(
+            "builtins.open", mock.mock_open(read_data="This is GDK_COMPONENT_NAME and GDK_COMPONENT_RECIPE_FILE")
+        ) as mock_file:
+            build_cmd.update_feature_files(Path("recipe.yaml"), Path("uat_recipe.yaml"))
+            assert mock_file.call_args_list == [
+                call(Path(".").joinpath("abc.feature"), "r", encoding="utf-8"),
+                call(Path(".").joinpath("abc.feature"), "w", encoding="utf-8"),
+            ]
+            assert mock_file.return_value.write.call_args_list == [call("This is abc and file:///abc.feature")]
+
+        assert mock_rglob.call_args_list == [call("*.feature")]
+
+    def test_update_features_with_component_recipe_file_without_build_raises_exception(self):
+        mock_rglob = self.mocker.patch("pathlib.Path.rglob", return_value=[Path("abc.feature")])
+        self.mocker.patch("pathlib.Path.exists", return_value=False)
+        self.mocker.patch("pathlib.Path.as_uri", return_value="file:///abc.feature")
+        build_cmd = BuildCommand({})
+
+        with mock.patch(
+            "builtins.open", mock.mock_open(read_data="This is GDK_COMPONENT_NAME and GDK_COMPONENT_RECIPE_FILE")
+        ) as mock_file:
+            with pytest.raises(Exception) as e:
+                build_cmd.update_feature_files(Path("recipe.yaml"), Path("uat_recipe.yaml"))
+                assert mock_file.call_args_list == [call(Path(".").joinpath("abc.feature"), "r", encoding="utf-8")]
+            assert "Component is not built" in e.value.args[0]
+
+        assert mock_rglob.call_args_list == [call("*.feature")]
+
+    def test_create_uat_recipe_from_build_recipe_without_replacing_s3_uri(self):
+        self.mocker.patch("pathlib.Path.exists", return_value=True)
+        self.mocker.patch("pathlib.Path.as_uri", return_value="file:///abc.feature")
+        build_cmd = BuildCommand({})
+        build_cmd.should_create_uat_recipe = True
+        test_recipe = {"manifests": [{"artifacts": [{"Uri": "docker://somefile.json"}]}]}
+        mock_read = self.mocker.patch(
+            "gdk.common.CaseInsensitive.CaseInsensitiveRecipeFile.read",
+            return_value=CaseInsensitiveDict(test_recipe),
+        )
+        spy_write = self.mocker.spy(CaseInsensitiveRecipeFile, "write")
+
+        build_cmd.create_uat_recipe_file(Path("recipe.yaml"), Path("uat_recipe.yaml"))
+        assert mock_read.call_args_list == [call(Path(".").joinpath("recipe.yaml"))]
+        assert spy_write.call_args_list == [call(ANY, Path("uat_recipe.yaml"), test_recipe)]
+
+    def test_create_uat_recipe_from_build_recipe_with_replacing_s3_uri(self):
+        self.mocker.patch("pathlib.Path.exists", return_value=True)
+
+        def get_as_uris(self):
+            return str(self)
+
+        self.mocker.patch.object(Path, "as_uri", get_as_uris)
+        build_cmd = BuildCommand({})
+        build_cmd.should_create_uat_recipe = True
+        test_recipe = {"manifests": [{"artifacts": [{"Uri": "s3://somefile.json"}]}]}
+        updated_recipe = {
+            "manifests": [
+                {
+                    "artifacts": [
+                        {
+                            "Uri": Path()
+                            .absolute()
+                            .joinpath("greengrass-build/artifacts/abc/1.0.0/somefile.json")
+                            .resolve()
+                            .as_uri()
+                        }
+                    ]
+                }
+            ]
+        }
+        mock_read = self.mocker.patch(
+            "gdk.common.CaseInsensitive.CaseInsensitiveRecipeFile.read",
+            return_value=CaseInsensitiveDict(test_recipe),
+        )
+
+        spy_write = self.mocker.spy(CaseInsensitiveRecipeFile, "write")
+        build_cmd.create_uat_recipe_file(Path("recipe.yaml"), Path("uat_recipe.yaml"))
+        assert mock_read.call_args_list == [call(Path(".").joinpath("recipe.yaml"))]
+        assert spy_write.call_args_list == [call(ANY, Path("uat_recipe.yaml"), updated_recipe)]
+
+    def test_create_uat_recipe_from_should_not_create_uat_recipe(self):
+        self.mocker.patch("pathlib.Path.exists", return_value=True)
+
+        def get_as_uris(self):
+            return str(self)
+
+        self.mocker.patch.object(Path, "as_uri", get_as_uris)
+        build_cmd = BuildCommand({})
+        build_cmd.should_create_uat_recipe = False
+        test_recipe = {"manifests": [{"artifacts": [{"Uri": "s3://somefile.json"}]}]}
+        mock_read = self.mocker.patch(
+            "gdk.common.CaseInsensitive.CaseInsensitiveRecipeFile.read",
+            return_value=CaseInsensitiveDict(test_recipe),
+        )
+        build_cmd.create_uat_recipe_file(Path("recipe.yaml"), Path("uat_recipe.yaml"))
+        assert not mock_read.call_args_list == [call(Path(".").joinpath("recipe.yaml"))]
+
+    def test_create_uat_recipe_when_artifact_does_not_exist_then_no_update_in_recipe(self):
+        self.mocker.patch("pathlib.Path.exists", return_value=False)
+
+        def get_as_uris(self):
+            return str(self)
+
+        self.mocker.patch.object(Path, "as_uri", get_as_uris)
+        build_cmd = BuildCommand({})
+        build_cmd.should_create_uat_recipe = True
+        test_recipe = {"manifests": [{"artifacts": [{"Uri": "s3://somefile.json"}]}]}
+        mock_read = self.mocker.patch(
+            "gdk.common.CaseInsensitive.CaseInsensitiveRecipeFile.read",
+            return_value=CaseInsensitiveDict(test_recipe),
+        )
+        spy_write = self.mocker.spy(CaseInsensitiveRecipeFile, "write")
+        build_cmd.create_uat_recipe_file(Path("recipe.yaml"), Path("uat_recipe.yaml"))
+        assert mock_read.call_args_list == [call(Path(".").joinpath("recipe.yaml"))]
+        assert spy_write.call_args_list == [call(ANY, Path("uat_recipe.yaml"), test_recipe)]
+
+    def test_build_uat_module(self):
+        uat_build_cmd = self.mocker.patch("subprocess.run", return_value=None)
+        self.mocker.patch("pathlib.Path.exists", return_value=True)
+        build_cmd = BuildCommand({})
+        build_cmd.build_uat_module()
+
+        if platform.system() == "Windows":
+            assert uat_build_cmd.call_args_list == [
+                call(
+                    ["mvn.cmd", "package"],
+                    check=True,
+                    cwd=Path().absolute().joinpath("greengrass-build/uat-features").resolve(),
+                )
+            ]
+        else:
+            assert uat_build_cmd.call_args_list == [
+                call(["mvn", "package"], check=True, cwd=Path().absolute().joinpath("greengrass-build/uat-features").resolve())
+            ]
