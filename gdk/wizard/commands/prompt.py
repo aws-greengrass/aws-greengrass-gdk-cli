7380fd46185fc2bd7983144276349feb8b052432
Jackson Zhang <jacksozh@amazon.com>
2023-07-13 18:55:02 +0000

    feat: created new folders and debugged prompting command for required fields

--- gdk/wizard/commands/prompt.py
+++ gdk/wizard/commands/prompt.py
@@ -6,14 +6,20 @@ from gdk.common.configuration import get_configuration
 from gdk.common.configuration import validate_configuration
 import gdk.common.utils as utils
 import argparse
+import sys
 import json  
 
 
 class Wizard:
     def __init__(self) -> None:
         # self.field_map is a dictionary object
-        self.field_map = get_configuration()
+        # self.field_map = get_configuration()
 
+        project_config_file = '/home/jacksozh/workspace/aws-greengrass-gdk-cli/gdk/wizard/static/dummy_config.json'
+        with open(project_config_file, "r") as config_file:
+            config_data = json.loads(config_file.read())
+
+        self.field_map = config_data
 
     """
     prompts the user of all required fields
@@ -36,65 +42,65 @@ class Wizard:
                                                 'cn-north-1', 'eu-central-1', 'eu-west-1', 'eu-west-2',
                                                 'us-gov-west-1', 'us-gov-east-1'], help='AWS region')
         parser.add_argument('--gdk_version', help='Version of the gdk cli tool compatible with the provided configuration')
-        parser.add_argument('--change-value', help='Change the value of a component field')
+        parser.add_argument('--change_value', help='Change the value of a component field')
         
     
             # check if there's already a value for that field, show the current value of that field
             # then prompt the user to answer (y/n) if they want to change the value in that field. 
             # if no break out of loop, if yes prompt them to change the value of the field
 
-        component_author = project_config[component_name]['author']
-        if component_author != "<PLACEHOLDER_AUTHOR>":
-            if self.change_value(parser, field='author', value=component_author):
-                while True:
-                    args = parser.parse_args(['--author', input('Enter the author of the component: ')])
-                    if self.check_input(args.author):
-                        self.field_map['component'][component_name]['author'] = args.author
-                        break
-                    print('Invalid author name. Please input again')
+        # component_author = project_config[component_name]['author']
+        # # if component_author != "<PLACEHOLDER_AUTHOR>":
+        # if self.change_value(parser, field='author', value=component_author):
+        #     while True:
+        #         args = parser.parse_args(['--author', input('Enter the author of the component: ')])
+        #         if self.check_input(args.author):
+        #             self.field_map['component'][component_name]['author'] = args.author
+        #             break
+        #         print('Invalid author name. Please input again.')
 
     
-        component_version = project_config[component_name]['version']
-        if component_version != "NEXT_PATCH":
-            if self.change_value(parser, field='version', value=component_version):
-                while True: 
-                    args = parser.parse_args(['--version', input('Enter the version of the component: ')])
-                    if self.check_input(args.version):
-                        self.field_map['component'][component_name]['version'] = args.version
-                        break
-                    print("Invalid version. Please input again")
+        # component_version = project_config[component_name]['version']
+        # # if component_version != "NEXT_PATCH":
+        # if self.change_value(parser, field='version', value=component_version):
+        #     while True: 
+        #         args = parser.parse_args(['--version', input('Enter the version of the component: ')])
+        #         if self.check_input(args.version):
+        #             self.field_map['component'][component_name]['version'] = args.version
+        #             break
+        #         print("Invalid version. Please input again.")
 
 
-        component_build_system = project_config[component_name]['build']['build_system']
-        if self.change_value(parser, field='build_system', value=component_build_system):
-            while True:
-                args = parser.parse_args(['--build_system', input('Enter the build system of the component: ')])
-                if self.check_input(args.build_system):
-                    self.field_map['component'][component_name]['build']['build_system'] = args.build_system
-                    break
-                print('Invalid build system name. Please input again')  
+        # component_build_system = project_config[component_name]['build']['build_system']
+        # if self.change_value(parser, field='build_system', value=component_build_system):
+        #     while True:
+        #         args = parser.parse_args(['--build_system', input('Enter the build system of the component: ')])
+        #         if self.check_input(args.build_system):
+        #             self.field_map['component'][component_name]['build']['build_system'] = args.build_system
+        #             break
+        #         print('Invalid build system name. Please input again.')  
 
 
 
-        component_bucket = project_config[component_name]['publish']['bucket']
-        if self.change_value(parser, field='bucket', value=component_bucket):
-            while True:
-                args = parser.parse_args(['--bucket', input('Enter the S3 bucket of the component: ')])
-                if self.check_input(args.bucket):
-                    self.field_map['component'][component_name]['publish']['bucket'] = args.bucket
-                    break
-                print('Invalid S3 bucket name. Please input again') 
+        # component_bucket = project_config[component_name]['publish']['bucket']
+        # if self.change_value(parser, field='bucket', value=component_bucket):
+        #     while True:
+        #         args = parser.parse_args(['--bucket', input('Enter the S3 bucket of the component: ')])
+        #         if self.check_input(args.bucket):
+        #             self.field_map['component'][component_name]['publish']['bucket'] = args.bucket
+        #             break
+        #         print('Invalid S3 bucket name. Please input again.') 
 
 
         
-        component_region = project_config[component_name]['publish']['region']
-        if self.change_value(parser, field='region', value=component_region):
-            while True:
-                args = parser.parse_args(['--bucket', input('Enter the region of the component: ')])
-                if self.check_input(args.region):
-                    self.field_map['component'][component_name]['publish']['region'] = args.region
-                    break
-                print('Invalid region. Please input again')
+        # component_region = project_config[component_name]['publish']['region']
+        # if self.change_value(parser, field='region', value=component_region):
+        #     while True:
+        #         args = parser.parse_args(['--region', input('Enter the region of the component: ')])
+        #         if self.check_input(args.region):
+        #             self.field_map['component'][component_name]['publish']['region'] = args.region
+        #             break
+        #         print('Invalid region. Please input again.')
 
 
         component_gdk_version = self.field_map['gdk_version'] 
@@ -104,20 +110,22 @@ class Wizard:
                 if self.check_input(args.gdk_version):
                     self.field_map['gdk_version'] = args.gdk_version
                     break
-                print('Invalid gdk-version. Please input again')        
+                print('Invalid gdk-version. Please input again.')        
 
 
 
 
-
+    """
+    A helper function for the prompt_required_fields method to
+    reduce repetiitve code
+    """
     def required_fields_helper(self, parser, field, value):
         if self.change_value(parser, field=field, value=value):
             while True:
                 args = parser.parse_args([f'--{field}', input(f'Enter the {field} of the component: ')])
                 if self.check_input(args.field):
                     return args.field #look into what the type is of args.field, specifically the type after args.
-                print('Invalid author name. Please input again')
-
+                print('Invalid author name. Please input again.')
         return value    
 
 
@@ -130,18 +138,20 @@ class Wizard:
     
 
     """
-    Check the user's inputs against the GDK-JSON file schema 
+    Check the user's inputs against the GDK-JSON file schema, returning True
+    for now
     """
     def check_input(self,input):
-        return
+        return True
     
 
     def change_value(self, parser, field, value):
         while True:
             args = parser.parse_args(['--change_value', input(f'Do you want to change the field {field} with value {value} ?(y/n)')])
-            if args.lower() in {'y', 'n'}:
+            if args.change_value.lower() in {'y', 'n'}:
                 break
-        return args.lower() == 'y'
+            print("Your input was not a valid response. Please respond again.")
+        return args.change_value.lower() == 'y'
 
 
     """
@@ -175,13 +185,16 @@ def get_schema():
     return schema
 
 def main():
-    # wiz = Wizard()
+    wiz = Wizard()
     # print(wiz.get_schema())
     # print('SCHEMA_KEYS:', list(get_schema().keys()))
     # print('SCHEMA_values:', list(get_schema().values()))
     # print(get_schema())
 
-    print(utils.current_directory)
+    # print(utils.current_directory)
+
+    wiz.prompt_required_fields()
+    print(wiz.field_map)
 
 if __name__ == "__main__":
     main()
