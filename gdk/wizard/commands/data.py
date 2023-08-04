6cc61ac0edc528766f55b2f33e40f4ddbd481830
Jackson Zhang <jacksozh@amazon.com>
2023-08-04 15:07:34 -0700

    feat: data refactoring

--- gdk/wizard/commands/data.py
+++ gdk/wizard/commands/data.py
@@ -6,8 +6,13 @@ import json
 
 class Wizard_data:
     def __init__(self):
-        self.project_config_file = _get_project_config_file()
-        self.field_map = get_configuration()
+        self.project_config_file = "/Users/jacksozh/Desktop/aws-greengrass-gdk-cli/gdk/wizard/static/default_config.json"
+        with open(self.project_config_file, "r") as config_file:
+            config_data = json.loads(config_file.read())
+        self.field_map = config_data
+
+        # self.project_config_file = _get_project_config_file()
+        # self.field_map = get_configuration()
         self.project_config = self.field_map["component"]
         self.component_name = next(iter(self.project_config))
         self.schema = self.get_schema()
@@ -31,3 +36,18 @@ class Wizard_data:
         with open(schema_file, "r") as schemaFile:
             schema = json.loads(schemaFile.read())
         return schema
+
+    def write_to_config_file(self):
+        """
+        Writes all the values in field_map to the gdk-config.json file
+
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            None
+        """
+        with open(self.project_config_file, "w", encoding="utf-8") as f:
+            f.write(json.dumps(self.field_map, indent=4))
