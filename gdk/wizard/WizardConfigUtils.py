c39945980a39298492b064185147f1e93d4aafca
Jackson Zhang <jacksozh@amazon.com>
2023-08-09 16:31:42 -0700

    chore: renamed WizardData to WizardConfigUtils

--- gdk/wizard/WizardConfigUtils.py
+++ gdk/wizard/WizardConfigUtils.py
@@ -2,14 +2,14 @@ import gdk.common.configuration as config
 import json
 
 
-class WizardData:
+class  WizardConfigUtils:
     def get_project_config_file(self):
         return config._get_project_config_file()
 
     def read_from_config_file(self):
         return config.get_configuration()
 
-    def write_to_config_file(self):
+    def write_to_config_file(self, config_file_path):
         """
         Writes all the values in field_map to the gdk-config.json file
 
@@ -21,5 +21,5 @@ class WizardData:
         -------
             None
         """
-        with open(self.project_config_file, "w", encoding="utf-8") as f:
+        with open(config_file_path, "w", encoding="utf-8") as f:
             f.write(json.dumps(self.field_map, indent=4))
