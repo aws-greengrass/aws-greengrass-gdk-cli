ea46aaa7169bd8d1499b80c2365eb7cf4327228b
Jackson Zhang <63770954+Jacksonzhang34@users.noreply.github.com>
2023-08-09 16:51:23 -0700

    feat: wizard enums and utils (#224)

---
+++ gdk/wizard/ConfigEnum.py
@@ -0,0 +1,28 @@
+from aenum import Enum, NoAlias
+
+
+class ConfigEnumDefault:
+    def __init__(self, key, default=None):
+        self.key = key
+        self.default = default
+
+
+class ConfigEnum(Enum):
+    # using NoAlias to avoid hash collisions
+
+    _settings_ = NoAlias
+    COMPONENT = ConfigEnumDefault("component")
+    COMPONENT_NAME = ConfigEnumDefault("component_name", default="gg-component")
+    AUTHOR = ConfigEnumDefault("author", default="gg-customer")
+    VERSION = ConfigEnumDefault("version", default="1.0.0")
+
+    BUILD = ConfigEnumDefault("build")
+    BUILD_SYSTEM = ConfigEnumDefault("build_system", "zip")
+    CUSTOM_BUILD_COMMAND = ConfigEnumDefault("custom_build_command")
+    BUILD_OPTIONS = ConfigEnumDefault("options", "{}")
+
+    PUBLISH = ConfigEnumDefault("publish")
+    BUCKET = ConfigEnumDefault("bucket", default="gg-component-bucket")
+    REGION = ConfigEnumDefault("region", default="us-east-1")
+    PUBLISH_OPTIONS = ConfigEnumDefault("options", "{}")
+    GDK_VERSION = ConfigEnumDefault("gdk_version", default="1.0.0")
---
+++ gdk/wizard/WizardConfigUtils.py
@@ -0,0 +1,25 @@
+import gdk.common.configuration as config
+import json
+
+
+class WizardConfigUtils:
+    def get_project_config_file(self):
+        return config._get_project_config_file()
+
+    def read_from_config_file(self):
+        return config.get_configuration()
+
+    def write_to_config_file(self, config_file_path):
+        """
+        Writes all the values in field_map to the gdk-config.json file
+
+        Parameters
+        ----------
+            None
+
+        Returns
+        -------
+            None
+        """
+        with open(config_file_path, "w", encoding="utf-8") as f:
+            f.write(json.dumps(self.field_map, indent=4))
