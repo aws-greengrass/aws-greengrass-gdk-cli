8cfaeafa32121aafb408d1334cbaeae3c3c6126f
Patrick Zhang <patrzhan@amazon.com>
2023-09-27 11:45:47 -0700

    test: modify unit testing for rebase

--- tests/gdk/wizard/test_Prompter.py
+++ tests/gdk/wizard/test_Prompter.py
@@ -1,16 +1,9 @@
 import pytest
 from unittest import TestCase
-<<<<<<< HEAD
-=======
 from unittest.mock import patch, Mock
->>>>>>> 55c0db9 (test: test prompter)
 from gdk.wizard.ConfigEnum import ConfigEnum
 from gdk.wizard.Prompter import Prompter
 from pathlib import Path
-
-
-class TestKeyboardInterrupt(Exception):
-    pass
 
 
 class TestPrompter(TestCase):
@@ -22,7 +15,6 @@ class TestPrompter(TestCase):
             "gdk.common.configuration._get_project_config_file",
             return_value=Path(".").joinpath("tests/gdk/static").joinpath("config.json"),
         )
-        self.Wizard = Prompter()
 
     def test_GIVEN_prompt_for_value_WHEN_new_value_provided_THEN_use_new_value(self):
         prompter = Prompter()
@@ -42,50 +34,34 @@ class TestPrompter(TestCase):
 
     @patch("builtins.input", side_effect=["y"])
     def test_change_configuration_positive(self, mock_input):
-        result = self.Wizard.change_configuration(ConfigEnum.BUILD)
+        prompter = Prompter()
+        result = prompter.change_configuration(ConfigEnum.BUILD)
         self.assertTrue(result)
         self.assertEqual(mock_input.call_count, 1)
         self.assertEqual(self.mock_get_project_config_file.call_count, 2)
 
     @patch("builtins.input", side_effect=["n"])
     def test_change_configuration_negative(self, mock_input):
-        result = self.Wizard.change_configuration(ConfigEnum.BUILD)
+        prompter = Prompter()
+        result = prompter.change_configuration(ConfigEnum.BUILD)
         self.assertFalse(result)
         self.assertEqual(mock_input.call_count, 1)
         self.assertEqual(self.mock_get_project_config_file.call_count, 2)
 
     @patch("builtins.input", side_effect=["invalid", "y"])
     def test_change_configuration_invalid_then_positive(self, mock_input):
-        result = self.Wizard.change_configuration(ConfigEnum.PUBLISH)
+        prompter = Prompter()
+        result = prompter.change_configuration(ConfigEnum.PUBLISH)
         self.assertTrue(result)
         self.assertEqual(mock_input.call_count, 2)
         self.assertEqual(self.mock_get_project_config_file.call_count, 2)
 
     @patch("builtins.input", side_effect=["invalid"] * 3)
     def test_change_configuration_max_attempts_reached(self, mock_input):
-        result = self.Wizard.change_configuration(ConfigEnum.PUBLISH)
+        prompter = Prompter()
+        result = prompter.change_configuration(ConfigEnum.PUBLISH)
         self.assertFalse(result)
         self.assertEqual(mock_input.call_count, 3)
-        self.assertEqual(self.mock_get_project_config_file.call_count, 2)
-
-    def test_interactive_prompt_interrupt(self):
-        mock_input = self.mocker.patch(
-            "gdk.wizard.Prompter.prompt", side_effect=KeyboardInterrupt
-        )
-        with self.assertRaises(Exception) as context:
-            self.Wizard.interactive_prompt(ConfigEnum.BUILD, "foo", True)
-
-        self.assertEqual(str(context.exception), "Wizard interrupted. Exiting...")
-        self.assertEqual(mock_input.call_count, 1)
-        self.assertEqual(self.mock_get_project_config_file.call_count, 2)
-
-    def test_interactive_prompt_valid(self):
-        mock_input = self.mocker.patch(
-            "gdk.wizard.Prompter.prompt", return_value={"user_input": "foo"}
-        )
-        result = self.Wizard.interactive_prompt(ConfigEnum.BUILD, "foo", True)
-        self.assertEqual(result, "foo")
-        self.assertEqual(mock_input.call_count, 1)
         self.assertEqual(self.mock_get_project_config_file.call_count, 2)
 
     def test_prompt_first_try(self):
@@ -96,8 +72,9 @@ class TestPrompter(TestCase):
             "gdk.wizard.WizardChecker.WizardChecker.is_valid_input", return_value=True
         )
 
-        self.Wizard.add_parser_arguments()
-        result = self.Wizard.prompter(ConfigEnum.BUILD_OPTIONS, required=False)
+        prompter = Prompter()
+        prompter.add_parser_arguments()
+        result = prompter.prompter(ConfigEnum.BUILD_OPTIONS, required=False)
 
         self.assertEqual(result, "new_value")  # User provided valid input
         self.assertEqual(mock_checker.call_count, 1)
@@ -114,8 +91,9 @@ class TestPrompter(TestCase):
             side_effect=[False, True],
         )
 
-        self.Wizard.add_parser_arguments()
-        result = self.Wizard.prompter(ConfigEnum.BUILD_OPTIONS, required=False)
+        prompter = Prompter()
+        prompter.add_parser_arguments()
+        result = prompter.prompter(ConfigEnum.BUILD_OPTIONS, required=False)
 
         self.assertEqual(result, "ok")  # User provided valid input
         self.assertEqual(mock_checker.call_count, 2)
@@ -132,8 +110,9 @@ class TestPrompter(TestCase):
             side_effect=[False, False, True],
         )
 
-        self.Wizard.add_parser_arguments()
-        result = self.Wizard.prompter(ConfigEnum.BUILD_OPTIONS, required=False)
+        prompter = Prompter()
+        prompter.add_parser_arguments()
+        result = prompter.prompter(ConfigEnum.BUILD_OPTIONS, required=False)
 
         self.assertEqual(result, "bar")  # User provided valid input
         self.assertEqual(mock_checker.call_count, 3)
@@ -153,8 +132,9 @@ class TestPrompter(TestCase):
             "gdk.wizard.WizardData.WizardData.get_field", return_value="old_value"
         )
 
-        self.Wizard.add_parser_arguments()
-        result = self.Wizard.prompter(ConfigEnum.PUBLISH_OPTIONS, required=False)
+        prompter = Prompter()
+        prompter.add_parser_arguments()
+        result = prompter.prompter(ConfigEnum.PUBLISH_OPTIONS, required=False)
 
         self.assertEqual(result, "old_value")  # User provided valid input
         self.assertEqual(mock_get_field.call_count, 1)
@@ -178,10 +158,11 @@ class TestPrompter(TestCase):
             "gdk.wizard.WizardConfigUtils.WizardConfigUtils.write_to_config_file",
             side_effect=Mock,
         )
-        self.Wizard.add_parser_arguments()
+        prompter = Prompter()
+        prompter.add_parser_arguments()
 
         with self.assertRaises(SystemExit):
-            self.Wizard.prompter(ConfigEnum.CUSTOM_BUILD_COMMAND, required=False)
+            prompter.prompter(ConfigEnum.CUSTOM_BUILD_COMMAND, required=False)
 
         self.assertEqual(mock_write_to_config_field.call_count, 1)
         self.assertEqual(mock_user_input.call_count, 3)
@@ -193,7 +174,7 @@ class TestPrompter(TestCase):
         # integration testing
         expected_field_dict = {
             "component": {
-                "1": {
+                "new.component.name": {
                     "author": "test_author",
                     "version": "1.0.0",
                     "build": {"build_system": "zip", "options": {}},
@@ -214,6 +195,7 @@ class TestPrompter(TestCase):
         mock_user_input = self.mocker.patch(
             "gdk.wizard.Prompter.Prompter.interactive_prompt",
             side_effect=[
+                "new.component.name",
                 "test_author",
                 "random_version",
                 "random_version",
@@ -226,9 +208,10 @@ class TestPrompter(TestCase):
                 "1.0.0",
             ],
         )
-        self.Wizard.prompt_fields()
+        prompter = Prompter()
+        prompter.prompt_fields()
         self.assertEqual(mock_change_configuration.call_count, 2)
-        self.assertEqual(mock_user_input.call_count, 10)
-        self.assertEqual(mock_user_input.call_count, 10)
-        self.assertEqual(expected_field_dict, self.Wizard.field_dict)
+        self.assertEqual(mock_user_input.call_count, 11)
+        self.assertEqual(mock_user_input.call_count, 11)
+        self.assertEqual(expected_field_dict, prompter.field_dict)
         self.assertEqual(self.mock_get_project_config_file.call_count, 2)
