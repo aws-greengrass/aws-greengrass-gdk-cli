28db1e3f090aa107f1151d3e0d729aa59fdb70b2
Patrick Zhang <patrzhan@amazon.com>
2023-10-24 13:39:56 -0700

    test: additional test case for no warning with new exclusion format

--- integration_tests/gdk/components/test_integ_BuildCommand.py
+++ integration_tests/gdk/components/test_integ_BuildCommand.py
@@ -72,6 +72,15 @@ class ComponentBuildCommandIntegTest(TestCase):
         assert "In GDK version 1.5.0, patterns for exclusions in zip builds" not in logs
         assert "[\"**/node_modules\", \"**/test*\", \"**/*.txt\"]" not in logs
 
+    def test_GIVEN_zip_build_system_WHEN_excludes_provided_with_new_patterns_THEN_no_warn_in_logs(self):
+        self.caplog.set_level(logging.WARNING)
+        self.zip_new_excludes_test_data()
+        bc = BuildCommand({})
+        bc.run()
+
+        logs = self.caplog.text
+        assert "In GDK version 1.5.0, patterns for exclusions in zip builds" not in logs
+
     def test_GIVEN_zip_build_system_WHEN_build_and_artifacts_not_on_s3_THEN_build_raises_exception(self):
         self.zip_test_data()
         content = CaseInsensitiveRecipeFile().read(self.tmpdir.joinpath("recipe.yaml"))
@@ -207,6 +216,25 @@ class ComponentBuildCommandIntegTest(TestCase):
         old_excludes_config_path = "integration_tests/test_data/config/config_old_excludes.json"
         shutil.copy(
             self.c_dir.joinpath(old_excludes_config_path), self.tmpdir.joinpath("gdk-config.json")
+        )
+
+        shutil.copy(
+            self.c_dir.joinpath("integration_tests/test_data/recipes/hello_world_recipe.yaml"),
+            self.tmpdir.joinpath("recipe.yaml"),
+        )
+        with open(self.tmpdir.joinpath("recipe.yaml"), "r") as f:
+            recipe = f.read()
+            recipe = recipe.replace("$GG_ARTIFACT", self.tmpdir.name + ".zip")
+
+        with open(self.tmpdir.joinpath("recipe.yaml"), "w") as f:
+            f.write(recipe)
+
+        self.tmpdir.joinpath("hello_world.py").touch()
+
+    def zip_new_excludes_test_data(self):
+        new_excludes_config_path = "integration_tests/test_data/config/config_new_excludes.json"
+        shutil.copy(
+            self.c_dir.joinpath(new_excludes_config_path), self.tmpdir.joinpath("gdk-config.json")
         )
 
         shutil.copy(
