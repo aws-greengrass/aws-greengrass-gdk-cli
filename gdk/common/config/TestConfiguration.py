4cf5322f411217025189a9e0af64f479c8654d99
patrzhan <139926810+patrzhan@users.noreply.github.com>
2023-12-06 12:05:15 -0800

    feat: add a suggestion to update GTF version if provided version is n… (#270)
    * feat: add a suggestion to update GTF version if provided version is not latest
    
    * feat: recommend using latest GTF version if old version in args
    
    * chore: move logic for GTF version suggestion

--- gdk/common/config/TestConfiguration.py
+++ gdk/common/config/TestConfiguration.py
@@ -1,4 +1,5 @@
 import logging
+from packaging.version import Version
 
 from gdk.common.GithubUtils import GithubUtils
 from gdk.common.consts import GTF_REPO_OWNER, GTF_REPO_NAME
@@ -9,6 +10,8 @@ class TestConfiguration:
         self.test_build_system = "maven"
         self.gtf_version = "1.2.0"  # Default value for when Github API call fails
         self.gtf_options = {}
+        self.upgrade_suggestion_already_provided = False
+        self.latest_gtf_version = None
 
         self._set_test_config(test_config)
 
@@ -22,9 +25,10 @@ class TestConfiguration:
     def _set_gtf_config(self, test_config):
         github_utils = GithubUtils()
         try:
-            latest_gtf_version = github_utils.get_latest_release_name(GTF_REPO_OWNER, GTF_REPO_NAME)
-            if latest_gtf_version is not None:
-                self.gtf_version = latest_gtf_version
+            release_name = github_utils.get_latest_release_name(GTF_REPO_OWNER, GTF_REPO_NAME)
+            if release_name is not None:
+                self.gtf_version = release_name
+                self.latest_gtf_version = release_name
                 logging.info("Discovered %s as latest GTF release name.", self.gtf_version)
             else:
                 logging.info("Unable to get the latest GTF release name. Using %s as the default value.", self.gtf_version)
@@ -35,6 +39,17 @@ class TestConfiguration:
         self.gtf_version = (test_config.get("gtf_version")
                             if "gtf_version" in test_config
                             else test_config.get("otf_version", self.gtf_version))
+        try:
+            # We have handling later to determine if user-provided version is incorrect, so if they are not proper
+            # versions to qualify for this warning, catch the error and just pass
+            if Version(self.gtf_version) < Version(self.latest_gtf_version):
+                logging.info(
+                    f"The current latest version of GTF is {self.latest_gtf_version}. Please consider updating your "
+                    "gdk-config.json to use the latest version."
+                )
+                self.upgrade_suggestion_already_provided = True
+        except Exception as e:
+            logging.debug("Not providing GTF update suggestion due to caught version error: %s", str(e))
         self.gtf_options = (test_config.get("gtf_options")
                             if "gtf_options" in test_config
                             else test_config.get("otf_options", {}))
